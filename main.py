6570bb3d001af95e11f20a09401331102e36752d
Igor Samarin <igorsamarin1986@gmail.com>
2024-03-20 11:51:25 +0300

    parameters have been added and the update has been adjusted

--- main.py
+++ main.py
@@ -5,33 +5,47 @@ import time
 from psutil._common import bytes2human
 
 
-class Counting(ft.Text):
-    def __init__(self, cpu_percent):
+class SystemParameters(ft.Text):
+    def __init__(self, parameter_number):
         super().__init__()
-        self.cpu_percent = cpu_percent
+        self.parameter_number = parameter_number
 
     def did_mount(self):
         self.running = True
-        self.page.run_task(self.update_cpu_loading)
+        self.page.run_task(self.update_parameters)
 
     def will_unmount(self):
         self.running = False
 
-    async def update_cpu_loading(self):
-        while self.cpu_percent:
-            cpu_load = psutil.cpu_percent(interval=1)
-            self.value = f"{cpu_load} %"
-            self.update()
+    async def update_parameters(self):
+        while self.running:
+            print(self.parameter_number)
+            if self.parameter_number == 1:
+                self.value = f"{psutil.cpu_percent(interval=1)} %"
+                self.update()
+            if self.parameter_number == 2:
+                self.value = f"{psutil.cpu_freq().current}"
+                self.update()
+            if self.parameter_number == 3:
+                self.value = f"{psutil.virtual_memory().total}"
+                self.update()
+            if self.parameter_number == 4:
+                self.value = f"{psutil.virtual_memory().available}"
+                self.update()
+            if self.parameter_number == 5:
+                self.value = f"{psutil.virtual_memory().used}"
+                self.update()
+            if self.parameter_number == 6:
+                self.value = f"{psutil.virtual_memory().free}"
+                self.update()
+
             await asyncio.sleep(0.2)
 
 
-gen_cpu = psutil.cpu_percent(interval=1)
 def main(page: ft.Page):
-    # page.add(Counting(gen_cpu))
-
     page.add(
         ft.DataTable(
-            width=700,
+            width=1700,
             bgcolor="brown",
             border=ft.border.all(2, "red"),
             border_radius=10,
@@ -46,20 +60,27 @@ def main(page: ft.Page):
             divider_thickness=0,
             column_spacing=200,
             columns=[
-                ft.DataColumn(ft.Text("CPU")),
-                ft.DataColumn(ft.Text("Second")),
-                ft.DataColumn(ft.Text("Diff"), numeric=True),
+                ft.DataColumn(ft.Text("CPU usage")),
+                ft.DataColumn(ft.Text("CPU frequency")),
+                ft.DataColumn(ft.Text("T"), numeric=True),
+                ft.DataColumn(ft.Text("T")),
+                ft.DataColumn(ft.Text("T")),
+                ft.DataColumn(ft.Text("T"), numeric=True),
             ],
             rows=[
                 ft.DataRow(
                     cells=[
-                        ft.DataCell(Counting(gen_cpu)),
-                        ft.DataCell(ft.Text("Test")),
-                        ft.DataCell(ft.Text("Test")),
+                        ft.DataCell(SystemParameters(1)),
+                        ft.DataCell(SystemParameters(2)),
+                        ft.DataCell(SystemParameters(3)),
+                        ft.DataCell(SystemParameters(4)),
+                        ft.DataCell(SystemParameters(5)),
+                        ft.DataCell(SystemParameters(6)),
                     ],
                 ),
             ],
         ),
     )
+
 
 ft.app(main)
